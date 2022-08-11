8e508a5bcc47e960f7d8630d17636631875d2275
anhquoc1301 <anhquoc1301@gmail.com>
2022-08-12 00:45:10 +0700

    update message

--- base/message.py
+++ base/message.py
@@ -3,8 +3,8 @@ from rest_framework import status
 
 def error(message='',data={}) :
 
-    return Response({"error": False,"message": message, "data":data},status=status.HTTP_200_OK)
+    return Response({"error": False,"message": message, "data":data},status=status.HTTP_400_BAD_REQUEST)
 
 def success(message='',data={}):
 
-    return Response({"success": True, "message": message, "data": data}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"success": True, "message": message, "data": data}, status=status.HTTP_200_OK)
